6b25b9cbf1c6c9f383d92a4671ecc370c3e46772
Ionel Tomi <ionel.tomi17@yahoo.com>
2020-11-05 19:58:46 +0200

    Add project files.

---
+++ Models/ExpenseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Tomi_Ionel_Lab5.Models
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
